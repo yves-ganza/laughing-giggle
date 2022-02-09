164ff72873a1d6aebd46678deef0c3c47a8d1548
YG <yg@YGs-MacBook-Air.local>
2022-02-08 22:51:26 -0500

    Initial commit

---
+++ contracts/Whitelist.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: Unlicense
+
+pragma solidity ^0.8.10;
+
+contract Whitelist {
+    uint8 public maxWhitelistAddresses;
+
+    mapping(address => bool) public whitelistedAddresses;
+
+    uint8 numOfAddressesWhitelisted;
+
+    constructor(uint8 _maxWhitelistedAddresses){
+        maxWhitelistAddresses = _maxWhitelistedAddresses;
+    }
+
+    function addAddressTowhitelist() public {
+        require(!whitelistedAddresses[msg.sender], 'Sender has already been registered');
+        require(numOfAddressesWhitelisted < maxWhitelistAddresses, 'Whitelist limit reached');
+
+        whitelistedAddresses[msg.sender] = true;
+
+        numOfAddressesWhitelisted +=1;
+    }
+}
